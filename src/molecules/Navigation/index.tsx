14efc551683737d33161c58e84634f7f460c8a15
Rizki36 <cahrizki.rf@gmail.com>
2022-04-20 14:30:54 +0700

    refactor: add link to landing page

--- src/molecules/Navigation/index.tsx
+++ src/molecules/Navigation/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import Hamburger from 'hamburger-react';
+import Link from 'next/link';
 
 import Button from '../../atoms/Button';
 import Container from '../../components/Container';
@@ -13,8 +14,12 @@ const Navigation = () => {
     <Container className="flex relative flex-col justify-between items-center text-blueMain bg-white md:flex-row">
       {/* title */}
       <div className="flex justify-between items-center w-full">
-        <h3 className="py-5 text-3xl font-medium">Laundry Kita</h3>
-        <div className="md:hidden">
+        <Link href={'/'} passHref>
+          <a>
+            <h3 className="py-5 text-3xl font-medium">Laundry Kita</h3>
+          </a>
+        </Link>
+        <div className="md:hidden" role={'button'} title="Button show menu">
           <Hamburger onToggle={() => setIsMenuOpened((v) => !v)} />
         </div>
       </div>
@@ -31,6 +36,7 @@ const Navigation = () => {
           rounded
           variant="outline-primary"
           className="py-1 my-8 md:my-0 md:ml-8"
+          title="Button login"
         >
           Login
         </Button>
