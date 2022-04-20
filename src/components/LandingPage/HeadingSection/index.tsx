76a8009549648764820822cb154d298c2e97ad07
Rizki36 <cahrizki.rf@gmail.com>
2022-04-20 14:02:59 +0700

    refactor: update text description and add link to about

--- src/components/LandingPage/HeadingSection/index.tsx
+++ src/components/LandingPage/HeadingSection/index.tsx
@@ -11,24 +11,30 @@ const HeadingSection = () => {
         className={`min-h-screen flex-1 flex justify-center items-center flex-col text-white`}
       >
         <h1 className="text-5xl font-bold">Laundry Kita</h1>
-        <p className="text-lg">
-          Aplikasi yang menyediakan layanan manajemen laundry untuk pemilik
-          usaha
+        <p className="text-lg text-center">
+          Aplikasi manajemen laundry untuk pemilik usaha laundry
         </p>
         <div className="mt-14">
           <Link href={'/check-status'} passHref>
             <Button
               variant="outline"
               title="Button Cek Status"
-              className="mr-4"
+              className="mr-4 w-full md:max-w-min"
               rounded
             >
               Cek Status
             </Button>
           </Link>
-          <Button variant="white" title="Button About Laundry Kita" rounded>
-            About Laundry Kita
-          </Button>
+          <Link href={'/about'}>
+            <Button
+              variant="white"
+              className="w-full md:max-w-min"
+              title="Button About Laundry Kita"
+              rounded
+            >
+              About Laundry Kita
+            </Button>
+          </Link>
         </div>
       </section>
     </Container>
