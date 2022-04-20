f4faa8d8534a70e9f687714dc7466d192530f57e
Rizki36 <cahrizki.rf@gmail.com>
2022-04-20 19:33:40 +0700

    refactor: change stapper into svg component

--- src/components/CheckStatusPage/BodySection/ResultHeader.tsx
+++ src/components/CheckStatusPage/BodySection/ResultHeader.tsx
@@ -1,37 +1,26 @@
-import { FC, memo } from 'react';
+import { FC } from 'react';
 
-import Step from '@/atoms/Step';
-
-const stepList = [
-  {
-    imgSrc: '/icons/disetorkan.svg',
-    titleProps: 'DISETORKAN',
-  },
-  {
-    imgSrc: '/icons/proses.svg',
-    titleProps: 'DIPROSES',
-  },
-  {
-    imgSrc: '/icons/siap-diambil.svg',
-    titleProps: 'SIAP DIAMBIL / DIKIRIM',
-  },
-  {
-    imgSrc: '/icons/selesai.svg',
-    titleProps: 'SELESAI',
-  },
-];
+import DeliveryStep from './Steps/DeliveryStep';
+import DepositeStep from './Steps/DepositeStep';
+import FinishStep from './Steps/FinishStep';
+import ProcessStep from './Steps/ProcessStep';
 
 const ResultHeader: FC<{ currentStep: number }> = ({ currentStep }) => {
-  const progress = stepList.map((step, index) => {
-    const thisStep = index + 1;
-    return (
-      <Step key={index} isDone={currentStep >= thisStep} imgSrc={step.imgSrc}>
-        {step.titleProps}
-      </Step>
-    );
-  });
-
-  return <ul className="flex justify-between w-full">{progress}</ul>;
+  return (
+    <>
+      <svg
+        id="Layer_1"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1328.13 248.13"
+      >
+        <FinishStep isActive={currentStep > 3} />
+        <DeliveryStep isActive={currentStep > 2} />
+        <ProcessStep isActive={currentStep > 1} />
+        <DepositeStep isActive={currentStep > 0} />
+      </svg>
+    </>
+  );
 };
 
-export default memo(ResultHeader);
+export default ResultHeader;
--- src/components/CheckStatusPage/BodySection/ResultState.tsx
+++ src/components/CheckStatusPage/BodySection/ResultState.tsx
@@ -8,13 +8,14 @@ const ResultState = () => {
   return (
     <>
       {/* detail */}
-      <div className="flex flex-col py-12 px-9 mb-8 w-full bg-gray-200 rounded-sm min-h-96">
+      <div className="flex flex-col justify-center py-12 px-9 mb-8 w-full bg-gray-200 rounded-sm ">
         <ResultHeader currentStep={currentStep} />
-        <div className="p-24 mt-10 h-24 text-center bg-gray-300 rounded-lg">
+        <div className="p-5 text-center bg-gray-300 rounded-lg lg:p-24 lg:mt-10">
           Cucian Anda selesai diproses dan siap diambil, apabila Anda memilih
           opsi Antar Cucian maka harap tunggu kurir mengantarkan cucian Anda
         </div>
       </div>
+
       {/* end of detail */}
     </>
   );
